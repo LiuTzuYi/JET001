5c8b7719eb2dfd21b0d709c4c70ada0a040d4afb
liutzuyi <liutzuyi@gmail.com>
2018-08-21 14:20:04 +0800

    模組 JWT001 的初始簽入

---
+++ Data/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace JWT001.Data
+{
+    public class ApplicationUser:IdentityUser
+    {
+        public ApplicationUser()
+        {
+        }
+    }
+}
---
+++ Data/SeedDatabase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace JWT001.Data
+{
+    public class SeedDatabase
+    {
+        
+        public static void Initialize(IServiceProvider serviceProvider){
+            var context = serviceProvider.GetRequiredService<ApplicationDbContext>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+            context.Database.EnsureCreated();
+
+            if (!context.Users.Any()){
+
+                ApplicationUser user = new ApplicationUser() { 
+                    Email="aaa@bbb.com",
+                    SecurityStamp=Guid.NewGuid().ToString(),
+                    UserName="Robert"
+                };
+                userManager.CreateAsync(user, "123456");
+            }
+        }
+    }
+}
